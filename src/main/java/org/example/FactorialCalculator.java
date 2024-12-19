6c90e2cab80918d4fd2709a424832daeeee3b352
LusyaSid <lusyas@tut.by>
2024-12-20 01:07:58 +0300

    TestNG

--- src/main/java/org/example/FactorialCalculator.java
+++ src/main/java/org/example/FactorialCalculator.java
@@ -1,19 +1,18 @@
 package org.example;
 
 public class FactorialCalculator {
-    public class CalculateFactorial {
-        public static long calculateFactorial(int n){
-            if (n<0){
+    public static long calculateFactorial(int n){
+        if (n<0){
                 throw new IllegalArgumentException("число не должно быть отрицательным");
-            }
-            if (n==0 ||n==1){
-                return 1;
-            }
-            long result=1;
-            for (int i = 2; i <= n; i++){
-                result *= i;
-            }
-            return result;
         }
+        if (n==0 ||n==1){
+            return 1;
+        }
+        long result=1;
+        for (int i = 2; i <= n; i++){
+            result *= i;
+        }
+        return result;
     }
 }
+
