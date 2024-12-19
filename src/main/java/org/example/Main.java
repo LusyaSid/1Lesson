5560e59ca2fc609a901053cb1cdced2c85aef624
LusyaSid <lusyas@tut.by>
2024-12-20 01:04:07 +0300

    TestNG

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        public class CalculateFactorial {
+            public static long calculateFactorial(int n){
+                if (n<0){
+                    throw new IllegalArgumentException("число не должно быть отрицательным");
+                }
+                if (n==0 ||n==1){
+                    return 1;
+                }
+                long result=1;
+                for (int i = 2; i <= n; i++){
+                    result *= i;
+                }
+                return result;
+            }
+        }
         //Первый метод
         printThreeWords();
         //Второй метод
