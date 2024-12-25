c4c803db73ee46dd592ca77195faafff0575dda2
LusyaSid <lusyas@tut.by>
2024-12-25 23:59:25 +0300

    Selenium_part_1

--- src/test/java/TestMts.java
+++ src/test/java/TestMts.java
@@ -13,9 +13,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 
-public class TestMts{
+public class TestMts {
 
     @Test
 
@@ -23,80 +24,86 @@ public class TestMts{
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
 
-        WebElement element=driver.findElement(By.xpath("//div[@id='pay-section']/div/div/div[2]/section/div/h2"));
-        String text=element.getText();
+        WebElement element = driver.findElement(By.xpath("//div[@id='pay-section']/div/div/div[2]/section/div/h2"));
+        String text = element.getText();
 
-        Assert.assertEquals(text,"Онлайн пополнение\n" + "без комиссии");
+        Assert.assertEquals(text, "Онлайн пополнение\n" + "без комиссии");
 
         driver.quit();
     }
 
     @Test
-    public void someTest1(){
-        WebDriver driver=new ChromeDriver();
+    public void someTest1() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-        WebElement element= driver.findElement(By.xpath("//img[@alt='Visa']"));
+        WebElement element = driver.findElement(By.xpath("//img[@alt='Visa']"));
         String logotip = element.getDomAttribute("alt");
-        Assert.assertEquals(logotip,"Visa");
+        Assert.assertEquals(logotip, "Visa");
         driver.quit();
     }
 
     @Test
-    public void someTest2(){
-        WebDriver driver=new ChromeDriver();
+    public void someTest2() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-        WebElement element=driver.findElement(By.xpath("//img[@alt='Verified By Visa']"));
+        WebElement element = driver.findElement(By.xpath("//img[@alt='Verified By Visa']"));
         String logotip = element.getDomAttribute("alt");
-        Assert.assertEquals(logotip,"Verified By Visa");
+        Assert.assertEquals(logotip, "Verified By Visa");
         driver.quit();
     }
 
     @Test
-    public void someTest3(){
-        WebDriver driver=new ChromeDriver();
+    public void someTest3() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-        WebElement element=driver.findElement(By.xpath("//img[@alt='MasterCard']"));
+        WebElement element = driver.findElement(By.xpath("//img[@alt='MasterCard']"));
         String logotip = element.getDomAttribute("alt");
-        Assert.assertEquals(logotip,"MasterCard");
+        Assert.assertEquals(logotip, "MasterCard");
         driver.quit();
     }
 
     @Test
-    public void someTest4(){
-        WebDriver driver=new ChromeDriver();
+    public void someTest4() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-        WebElement element=driver.findElement(By.xpath("//img[@alt='MasterCard Secure Code']"));
+        WebElement element = driver.findElement(By.xpath("//img[@alt='MasterCard Secure Code']"));
         String logotip = element.getDomAttribute("alt");
-        Assert.assertEquals(logotip,"MasterCard Secure Code");
+        Assert.assertEquals(logotip, "MasterCard Secure Code");
         driver.quit();
     }
 
     @Test
-    public void someTest5(){
-        WebDriver driver=new ChromeDriver();
+    public void someTest5() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-        WebElement element=driver.findElement(By.xpath("//img[@alt='Белкарт']"));
+        WebElement element = driver.findElement(By.xpath("//img[@alt='Белкарт']"));
         String logotip = element.getDomAttribute("alt");
-        Assert.assertEquals(logotip,"Белкарт");
+        Assert.assertEquals(logotip, "Белкарт");
         driver.quit();
     }
 
     @Test
-    public void someTest6(){
-        WebDriver driver=new ChromeDriver();
+    public void someTest6() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-        WebElement element=driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/a"));
+
+
+        WebElement element = driver.findElement(By.xpath("//div[@class='pay__wrapper']//a"));
         element.click();
-        String link= driver.getCurrentUrl();
-        Assert.assertEquals(link,"https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/");
+        String link = driver.getCurrentUrl();
+        Assert.assertEquals(link, "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/");
         driver.quit();
+
     }
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
 
 
 
